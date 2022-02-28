51c2a9955475ddb5adb5bbd7001cea567bb91d96
KirilMihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2022-02-28 22:12:31 +0200

    commit: Added Employees wrapper class

---
+++ src/database/employees.py
@@ -0,0 +1,36 @@
+from typing import Any, Dict, List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.expression import select
+
+from .model.employees import Employees as EmployeesModel
+from .model.users import Users as UsersModel
+
+
+class Employees(object):
+    async def all_by_place(session: AsyncSession, id: int) -> List[Dict[str, Any]]:
+        return list(
+            map(
+                lambda result: result._asdict(),
+                (
+                    await session.execute(
+                        select(
+                            [
+                                UsersModel.first_name,
+                                UsersModel.last_name,
+                                UsersModel.email,
+                                UsersModel.profile_picture,
+                                EmployeesModel.added_by,
+                                EmployeesModel.can_reward,
+                            ],
+                            from_obj=EmployeesModel,
+                        ).join(
+                            UsersModel,
+                            EmployeesModel.id == UsersModel.id,
+                        ).where(
+                            EmployeesModel.place_id == id
+                        ),
+                    )
+                ).all(),
+            ),
+        )
