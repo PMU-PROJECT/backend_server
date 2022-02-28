693a5014590101ce63fdc48c65268dad2b979428
CreeperBeatz <dani.matev123@gmail.com>
2022-02-28 23:45:46 +0200

    test: testing helper methods

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ import json
 from quart import Quart, request
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.app_logic import get_tourist_sites
+
 # Own imports
 from .config.logger_config import logger
 from .utils.enviromental_variables import PORT
@@ -18,18 +20,18 @@ application.before_serving(db_init)
 
 ###### REQUEST HANDLERS ######
 
-@application.route('/get_tourist_sites_info', methods=['POST'])
+@application.route('/get_tourist_sites_info', methods=['GET'])
 async def interactive():
-    data = json.loads(request.form.get('payload'))
-
+    data = request.form
     # TODO check token
 
     logger.debug("User requested site info")
 
     async with async_session() as session:
         session: AsyncSession
+        sites = await get_tourist_sites(session)
 
-    return {}, 200
+    return sites, 200
 
 
 @application.route('/reset', methods=['POST'])
@@ -40,7 +42,7 @@ def reset():
 
 ###### WEB SERVER START ######
 def manual_run():
-    application.run(host='0.0.0.0', port=PORT)
+    application.run(host='0.0.0.0', port=PORT, debug=True)
 
 
 # Put anything that you want to start from Gunicorn master proccess here
--- src/app_logic.py
+++ src/app_logic.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .database.model.places import Places
-from .database.model.employees import Employees
-from .database.model.images import Images
-from src.database.model import employees
+from .database.places import Places
+from .database.employees import Employees
+from .database.images import Images
 
 
-def get_tourist_sites(session: AsyncSession):
+async def get_tourist_sites(session: AsyncSession):
     '''
     DUMMY FUNCTION
     shows how to create logic for program
@@ -15,13 +14,17 @@ def get_tourist_sites(session: AsyncSession):
     get tourist sites info
     return it
     '''
-    all_sites = Places.all(session)
+    all_sites = await Images.all_by_place(session, 1)
+    print(all_sites)
+    print(type(all_sites))
 
+    '''
     for i, site in enumerate(all_sites):
         images = Images.all_by_place(site.get('id'))
         employees = Employees.all_by_place(site.get('id'))
 
         all_sites[i]['images'] = images
         all_sites[i]['employees'] = employees
+    '''
 
     return all_sites
