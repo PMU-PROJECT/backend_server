48ede8eca1991384e10467f6cde381835ba326ef
CreeperBeatz <dani.matev123@gmail.com>
2022-02-28 22:59:46 +0200

    feat: added description column on places

--- src/database/model/places.py
+++ src/database/model/places.py
@@ -1,19 +1,21 @@
-from sqlalchemy import Text
+from sqlalchemy import TEXT
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Numeric, Unicode
 
 from . import ORMBase, id_column, id_ref_column
-from .cities import Cities
+from .regions import Regions
 
 
 class Places(ORMBase):
-    city_id = id_ref_column('city_id', Cities.id,)
+    region_id = id_ref_column('region_id', Regions.id,)
+
+    city_id = id_ref_column('city_id', Regions.id,)
 
     id = id_column('place_id',)
 
     name = Column('name', Unicode(255,),)
 
-    description = Column('description', Text(),)
+    description = Column('description', TEXT())
 
     latitude = Column(
         'latitude',
