0ee92dfb3163a1e8173f37ec71260762e43133b9
CreeperBeatz <dani.matev123@gmail.com>
2022-03-05 16:39:36 +0200

    feat: add prototyped endpoints

--- src/app.py
+++ src/app.py
@@ -19,19 +19,108 @@ application.before_serving(db_init)
 
 ###### API REQUEST HANDLERS ######
 
+# Tourist site endpoints
+
 @application.route('/api/get_all_sites', methods=['GET'])
 async def get_all_sites():
     args = request.args
     headers = request.headers
     # TODO check token
 
-    logger.debug("User requested site info")
+    logger.debug("User requested all site info")
 
     async with async_session() as session:
         session: AsyncSession
         sites = await get_tourist_sites(session)
 
     return sites, 200
+
+
+@application.route('/api/get_site_info', methods=['GET'])
+async def get_site_info():
+    args = request.args
+    headers = request.headers
+    # TODO check token
+
+    logger.debug("User requested site detailed info")
+
+    async with async_session() as session:
+        session: AsyncSession
+        site = {'site': None}  # TODO change
+
+    return site, 200
+
+# Login and registration endpoints
+
+
+@application.route('/api/registration', methods=['POST'])
+async def registration():
+    form = request.json
+
+    first_name = form.get('first_name')
+    last_name = form.get('last_name')
+    email = form.get('email')
+    password = form.get('password')
+
+    if None in (first_name, last_name, email, password):
+        return 'insufficient information', 422
+
+    # TODO request to DB for registration
+    # TODO generate JWT token and return it
+
+    return {'token': 'yes'}
+
+
+@application.route('/api/login', methods=['POST'])
+async def login():
+    form = request.json
+
+    email = form.get('email')
+    password = form.get('password')
+
+    if None in (email, password):
+        return 'insufficient information', 422
+
+    # TODO check if user exists
+    # TODO return token if yes
+    # TODO return error code if no
+
+    return {'token': 'yes'}
+
+
+@application.route('/api/google_login', methods=['POST'])
+async def google_login():
+    form = request.json
+
+    google_token = form.get('token')
+    password = form.get('password')
+
+    if google_token is None:
+        return 'insufficient information', 422
+
+    # TODO API call to google for user info
+    # TODO if not valid return error code
+
+    # TODO if valid, check if user exists locally
+    # TODO return token if yes
+    # TODO make user and return token if no
+
+    return {'token': 'yes'}
+
+# User info endpoint
+
+
+@application.route('api/get_user_info', methods=['GET'])
+async def get_user_info():
+    headers = request.headers
+
+    # TODO decode token, if valid and user exists
+    # TODO return user info
+    # TODO else return error code
+
+    async with async_session() as session:
+        session: AsyncSession
+        sites = await get_tourist_sites(session)
 
 ###### IMAGE SERVER HANDLERS ######
 
