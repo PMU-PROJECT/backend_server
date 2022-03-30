2125ad90a911670acd5e1bc2c2db598918205da8
KirilMihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2022-03-30 18:42:34 +0300

    commit: Formatting & new "first_by_place" method

--- src/database/images.py
+++ src/database/images.py
@@ -1,22 +1,22 @@
-from typing import List
+from typing import List, Optional
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql.expression import select, literal
+from sqlalchemy.sql.expression import literal, select
 
 from .model.images import Images as ImagesModel
 
 
 class Images(object):
     @staticmethod
+    async def first_by_place(session: AsyncSession, place_id: int) -> Optional[str]:
+        return (await session.execute(
+            select(
+                [ImagesModel.filename, ], ).where(
+                ImagesModel.place_id == literal(place_id), ), )).scalar()
+
+    @staticmethod
     async def all_by_place(session: AsyncSession, place_id: int) -> List[str]:
-        return await (
-            await session.stream_scalars(
-                select(
-                    [
-                        ImagesModel.filename,
-                    ],
-                ).where(
-                    ImagesModel.place_id == literal(place_id, ),
-                ),
-            )
-        ).all()
+        return (await session.execute(
+            select(
+                [ImagesModel.filename, ], ).where(
+                ImagesModel.place_id == literal(place_id, ), ), )).scalars().all()
