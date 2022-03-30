4eeb7125d01028a6c1c4cb6175cd19ace3a2b01c
KirilMihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2022-03-30 18:39:10 +0300

    commit: Added rewards log ORM wrapper class

---
+++ src/database/rewards_log.py
@@ -0,0 +1,48 @@
+from typing import Any, Dict, List
+
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.expression import select, insert
+
+from .model.rewards_log import RewardsLog as RewardsLogModel
+from ..exceptions import DatabaseError
+
+
+class RewardsLog(object):
+    @staticmethod
+    async def all_by_visitor_id(session: AsyncSession, visitor_id: int) -> List[Dict[str, Any]]:
+        try:
+            return list(
+                map(
+                    lambda result: result._asdict(), (
+                        await session.execute(
+                            select(
+                                [RewardsLogModel, ],
+                            ).where(
+                                RewardsLogModel.visitor_id == visitor_id,
+                            ),
+                        )
+                    ).all(),
+                ),
+            )
+        except Exception as ex:
+            raise DatabaseError(ex)
+
+    @staticmethod
+    async def insert(session: AsyncSession, visitor_id: int, reward_id: int, employee_id: int) -> bool:
+        try:
+            await session.execute(
+                insert(
+                    RewardsLogModel,
+                ).values(
+                    visitor_id=visitor_id,
+                    reward_id=reward_id,
+                    employee_id=employee_id,
+                )
+            )
+        except IntegrityError:
+            return False
+        except Exception as ex:
+            raise DatabaseError(ex)
+
+        return True
