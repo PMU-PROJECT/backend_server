03ef69900922e3d03217280ab460a6881f80053f
KirilMihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2022-03-30 18:41:50 +0300

    minor: Formatting, import optimization & type annotations

--- src/database/places.py
+++ src/database/places.py
@@ -1,9 +1,8 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union
 
 from sqlalchemy.engine import Row
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import Join
-from sqlalchemy.sql.expression import select
+from sqlalchemy.sql.expression import select, Select
 
 from .model.cities import Cities as CitiesModel
 from .model.places import Places as PlacesModel
@@ -12,35 +11,19 @@ from .model.regions import Regions as RegionsModel
 
 class Places(object):
     @staticmethod
-    def __query() -> Join:
+    def __query() -> Select:
         return select(
-            [
-                RegionsModel.name.label('region_name'),
-                CitiesModel.name.label('city_name'),
-                PlacesModel.id,
-                PlacesModel.name.label('name'),
-                PlacesModel.description,
-                PlacesModel.latitude,
-                PlacesModel.longitude,
-            ],
-            from_obj=PlacesModel,
-        ).join(
-            CitiesModel,
-            PlacesModel.city_id == CitiesModel.id,
-        ).join(
-            RegionsModel,
-            CitiesModel.region_id == RegionsModel.id,
-        )
+            [RegionsModel.name.label('region_name'), CitiesModel.name.label('city_name'), PlacesModel.id,
+                PlacesModel.name.label('name'), PlacesModel.description, PlacesModel.latitude, PlacesModel.longitude, ],
+            from_obj=PlacesModel, ).join(
+            CitiesModel, PlacesModel.city_id == CitiesModel.id, ).join(
+            RegionsModel, CitiesModel.region_id == RegionsModel.id, )
 
     @staticmethod
-    async def by_id(session: AsyncSession, place_id: int) -> Union[None, Dict[str, Any]]:
-        result: Union[None, Row] = (
-            await session.execute(
-                Places.__query().where(
-                    PlacesModel.id == place_id,
-                ),
-            )
-        ).first()
+    async def by_id(session: AsyncSession, place_id: int) -> Optional[Dict[str, Any]]:
+        result: Union[None, Row] = (await session.execute(
+            Places.__query().where(
+                PlacesModel.id == place_id, ), )).first()
 
         return None if result is None else result._asdict()
 
@@ -48,11 +31,5 @@ class Places(object):
     async def all(session: AsyncSession) -> List[Dict[str, Any]]:
         return list(
             map(
-                lambda result: result._asdict(),
-                await (
-                    await session.stream(
-                        Places.__query(),
-                    )
-                ).all(),
-            ),
-        )
+                lambda result: result._asdict(), await (await session.stream(
+                    Places.__query(), )).all(), ), )
